85912d9fc035465a4470183a45311fe83532688f
prabalrai-git <prabalrai17@gmail.com>
2023-02-13 15:18:36 +0545

    changes

--- src/Services/constants/url.js
+++ src/Services/constants/url.js
@@ -7,7 +7,7 @@ const API_URL = `https://lunivacare.ddns.net/bakeryProduction/`;
  * @desc: api base url with route
  *
  */
-export const BASE_URL = `${API_URL}LunivaproductionApi/`;
+  export const BASE_URL = `${API_URL}LunivaproductionApi/`;
 
 export const GetItemList = "GetItemList";
 
